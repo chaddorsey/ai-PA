b1ce88485cbbd0998d8456b8000b8cf2c76f7470
Chad Dorsey <chaddorsey@gmail.com>
2025-11-18 13:55:54 -0500

    Adding init file

---
+++ letta/scheduling_orchestrator/__init__.py
@@ -0,0 +1,7 @@
+"""
+Scheduling Orchestrator for Letta
+
+A high-leverage scheduling orchestration tool that uses DSPy for natural language
+understanding and clingo ASP for constraint-based optimization on a 15-minute grid.
+"""
+
