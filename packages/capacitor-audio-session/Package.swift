cd3a2e04d800f1bd58d536cea5fe8b9508197636
Chad Dorsey <cdorsey@concord.org>
2026-06-29 05:55:53 -0400

    fix(native): lower CapacitorAudioSession pod deployment target 14->13 (was higher than app target -> pod install incompatibility)

--- packages/capacitor-audio-session/Package.swift
+++ packages/capacitor-audio-session/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorAudioSession",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "CapacitorAudioSession",
